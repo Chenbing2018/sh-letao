ce4715c949b30198d79fe927cd041ca9c3ebb1d3
chenbing <1042613792@qq.com>
2018-06-25 19:17:56 +0800

    登录界面完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,8 +1,16 @@
 
-// 1.登录页面表单效验
+
 $(function () {
+  // 1.登录页面表单效验
   // 表单校验初始化
   $('#form').bootstrapValidator({
+
+    // 配置图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',   // 校验成功
+      invalid: 'glyphicon glyphicon-remove', // 校验失败
+      validating: 'glyphicon glyphicon-refresh'  // 校验中
+    },
     // 指定校验字段
     fields: {
       username: {
@@ -17,6 +25,9 @@ $(function () {
             min: 4,
             max: 10,
             message: "用户名长度必须在 4-10位"
+          },
+          callback: {
+            message: '此用户名不存在'
           }
         }
       },
@@ -32,9 +43,42 @@ $(function () {
             min: 6,
             max: 12,
             message: "密码长度必须在 6-12位"
+          },
+          callback: {
+            message: '密码错误'
           }
         }
       }
     }
   });
+
+  // 2.表单提交按钮, 阻止,用ajax请求
+  $('#form').on("success.form.bv", function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'post',
+      url: '/employee/employeeLogin',
+      data: $('#form').serialize(),
+      dataType: 'json',
+      success: function (info) {
+        console.log(info);
+        if (info.success) {
+          location.href = "index.html";
+        }
+        if (info.error == 1000) {
+          console.log('用户名不存在');
+          $('#form').data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback');
+        }
+        if (info.error == 1001) {
+          console.log('密码错误');
+          $('#form').data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback');
+        }
+      }
+    })
+  });
+
+  // 3.表单重置按钮
+  $('[type="reset"]').click(function () {
+    $('#form').data('bootstrapValidator').resetForm();
+  })
 })
