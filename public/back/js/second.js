df00779fffc8aba47aa03cff34fd4e124409e7f5
chenbing <1042613792@qq.com>
2018-06-27 14:30:28 +0800

    二级分类完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -18,7 +18,6 @@ $(function () {
       },
       dataType: 'json',
       success: function (info) {
-        console.log(info);
         var htmlStr = template('tmp', info);
         $('tbody').html(htmlStr);
         // 分页
@@ -38,5 +37,118 @@ $(function () {
   // 2.点击添加分类按钮,弹出模态框
   $('#addBtn').click(function () {
     $('#addModal').modal('show');
+    // 动态生成下拉框
+    $.ajax({
+      type: 'get',
+      url: '/category/queryTopCategoryPaging',
+      data: {
+        page: 1,
+        pageSize: 50
+      },
+      success: function (info) {
+        $('.dropdown-menu').html(template('tpl', info));
+      }
+    })
   })
+
+  // 3.事件委托,点击下拉菜单的选项并赋值给一级分类内容
+  $('.dropdown-menu').on('click', 'li a', function () {
+    var txt = $(this).text();
+    $('#firstCate').text(txt);
+
+    var categoryId = $(this).data('id');
+    $('#categoryId').val(categoryId);
+  })
+
+  // 4.上传图片
+  $("#fileUpload").fileupload({
+    dataType:"json",
+    //e：事件对象
+    //data：图片上传后的对象，通过e.result.picAddr可以获取上传后的图片地址
+    done:function (e, data) {
+      //console.log(data.result.picAddr);
+      var src = data.result.picAddr;
+      console.log(data.result.picAddr);
+      $('#brandLogo').val(src);
+      $('#imgBox img').attr('src', src);
+    }
+  });
+
+  // 5.表单效验
+  $('#form').bootstrapValidator({
+    excluded: [],
+    // 配置图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',   // 校验成功
+      invalid: 'glyphicon glyphicon-remove', // 校验失败
+      validating: 'glyphicon glyphicon-refresh'  // 校验中
+    },
+    fields: {
+      categoryId: {
+        validators: {
+          notEmpty: {
+            message: '必须选择一级分类'
+          }
+        }
+      },
+      brandName: {
+        validators: {
+          notEmpty: {
+            message: '必须输入二级分类'
+          }
+        }
+      },
+      brandLogo: {
+        validators: {
+          notEmpty: {
+            message: '必须上传图片'
+          }
+        }
+      },
+    }
+  })
+
+  // 6.表单效验成功后,组织默认提交进行ajax提交
+  $('#form').on('success.form.bv', function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'post',
+      url: '/category/addSecondCategory',
+      data: $('#form').serialize(),
+      dataType: 'json',
+      success: function () {
+        // 关闭模态框
+        $('#addModal').modal('hide');
+        // 重置表单
+        $('#form').data("bootstrapValidator").resetForm(true);
+        // 重置一级分类和图片
+        $('#firstCate').text('请输入一级分类');
+        $('#imgBox img').attr('src', 'images/none.png');
+        // 重新渲染
+        currentPage = 1;
+        render();
+      }
+    })
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
